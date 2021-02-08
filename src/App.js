2c48da81716a75ed7391e86d6644f1a385b9a8ec
dbyilla69 <doranyilla@gmail.com>
2021-02-08 12:53:58 -0600

    small changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
 
-
+// import { Switch, Route } from 'react-router-dom';
+//     <Switch>
+//       <Route exact path={'/'} component={comp} />
+//       <Route exact path={'/add'} component={comp} />
+//     </Switch>;
 const App = () => (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App With Material-UI
-        </Typography>
-      </Box>
-    </Container>
-  );
-
+  <Container maxWidth='sm'>
+    <Box my={4}>
+      <Typography variant='h4' component='h1' gutterBottom>
+        Create React App With Material-UI
+      </Typography>
+    </Box>
+  </Container>
+);
 
 export default App;
